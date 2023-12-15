f8cfdc748937417ee5fce4bf26d8022018fca4c3
vorapat0881 <vorapat_g@cmu.ac.th>
2023-12-15 17:04:11 +0700

    lab9_2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N, int M){
+	if (M <= 0 || N <=0 ) cout << "Invalid input";
 
+	else {for(int i = 0; i < N; i++){
+	
+		for(int j = 0; j < M; j++){
+			cout << "O";
+			
+		}
+	cout << "\n";
+	}
+	
+}
+}
 int main(){
 	
 	printO(2,2);
